2d1dba10c2ad28fd338835879e227682d956aff1
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-04 20:23:40 -0400

    chore(init): reorder hooks per hook_api.md lifecycle order

--- init.zsh
+++ init.zsh
@@ -1,40 +1,11 @@
 # shellcheck shell=bash
 ######################################################################
-#<
-#
-# Function: p6df::modules::vim::deps()
-#
-#>
-######################################################################
 p6df::modules::vim::deps() {
   ModuleDeps=(
     p6m7g8-dotfiles/p6common
   )
 }
 
-######################################################################
-#<
-#
-# Function: p6df::modules::vim::external::brews()
-#
-#>
-######################################################################
-p6df::modules::vim::external::brews() {
-
-  p6df::core::homebrew::cli::brew::install vim
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::vim::aliases::init(dir)
-#
-#  Args:
-#	dir -
-#
-#>
 ######################################################################
 p6df::modules::vim::aliases::init() {
   local _module="$1"
@@ -46,13 +17,6 @@ p6df::modules::vim::aliases::init() {
 }
 
 ######################################################################
-#<
-#
-# Function: p6df::modules::vim::home::symlinks()
-#
-#  Environment:	 HOME P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
-#>
-######################################################################
 p6df::modules::vim::home::symlinks() {
 
   p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-vim/share/vimrc" "$HOME/.vimrc"
@@ -61,6 +25,48 @@ p6df::modules::vim::home::symlinks() {
   p6_return_void
 }
 
+######################################################################
+p6df::modules::vim::external::brews() {
+
+  p6df::core::homebrew::cli::brew::install vim
+
+  p6_return_void
+}
+
+######################################################################
+p6df::modules::vim::profile::mod() {
+
+  p6_return_words 'vim' '$VIMINIT'
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::vim::deps()
+#
+#>
+######################################################################
+#<
+#
+# Function: p6df::modules::vim::external::brews()
+#
+#>
+######################################################################
+#<
+#
+# Function: p6df::modules::vim::aliases::init(dir)
+#
+#  Args:
+#	dir -
+#
+#>
+######################################################################
+#<
+#
+# Function: p6df::modules::vim::home::symlinks()
+#
+#  Environment:	 HOME P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
+#>
 ######################################################################
 #<
 #
@@ -71,9 +77,3 @@ p6df::modules::vim::home::symlinks() {
 #
 #  Environment:	 VIMINIT
 #>
-######################################################################
-p6df::modules::vim::profile::mod() {
-
-  p6_return_words 'vim' '$VIMINIT'
-}
-
